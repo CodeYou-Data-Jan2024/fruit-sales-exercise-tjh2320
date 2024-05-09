b7e0e82fdc3849368adb0ba25d131fa7ea16fb25
Thomas Hauber <thomashauber01@gmail.com>
2024-05-09 13:49:10 -0500

    Fuit Sales assignment

--- fruitsales.py
+++ fruitsales.py
@@ -1,1 +1,15 @@
-# add your code here
+import pandas as pd
+fruits = pd.DataFrame(columns = ["Apples", "Bananas"])
+
+fruits_data = {
+    "Apples" : [35],
+    "Bananas" : [21] 
+}
+pd.DataFrame(fruits_data)
+
+fruit_sales = pd.DataFrame({
+    "Apples" : [35, 41],
+    "Bananas" : [21, 34]},
+    index = ["2017 Sales", "2018 Sales"])
+
+fruit_sales.to_csv("fruit.csv", sep = ',', encoding = "utf-8", index = True)
